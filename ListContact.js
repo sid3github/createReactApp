5e13a5e23bc7da41961c758ea5bcb3fa1ded03cd
mohammed afzal <mohammedafzal94@gmail.com>
2018-06-29 11:55:59 +0530

    class component

---
+++ ListContact.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class ListContacts extends React.Component{
+    render(){
+        console.log(this.props);
+        return(
+            <ol>
+            </ol>
+        )
+    }
+}
+
+module.exports = ListContacts;
